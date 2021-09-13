7775a9844ee7bcb8455ffdca1980a847ad637bd0
rizki nabil <rizki.nbl123@gmail.com>
2021-09-13 17:00:51 +0700

    workspace clean up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import react from "react";
+
+import "./assets/scss/style.scss"
+
+function App() {
+  return (
+    <div className="App">
+      Hello world!
+    </div>
+  );
+}
+
+export default App;
