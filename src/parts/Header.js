b358cac3c9b90b498feb480a36bf68fc3b3f8b4c
rizki nabil <rizki.nbl123@gmail.com>
2021-11-11 16:03:41 +0700

    update

---
+++ src/parts/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Button from "elements/Button";
+
+export default function Header(props) {
+   return (
+      <div>
+         <header className="spaceing-sm">
+            <div className="container">./</div>
+         </header>
+      </div>
+   );
+}
---
+++ src/parts/IconText.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Button from "elements/Button";
+
+export default function IconText() {
+   return (
+      <Button className="brand-text-icon" href="" type="link">
+         Emsa<span className="text-gray-900"> Epsilon.</span>
+      </Button>
+   );
+}
